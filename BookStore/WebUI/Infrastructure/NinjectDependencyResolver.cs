5ccfb8084093dfb148764bee2f1566ab623ee8b5
fil-eadr <d.filonenko@eadr.com.ua>
2016-06-01 12:44:04 +0300

    Create structure book store

---
+++ BookStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book {Name = "книга 1", Author = "автор 1", Price = 100},
+                new Book { Name = "книга 2", Author = "автор 2", Price = 200 },
+                new Book { Name = "книга 3", Author = "автор 3", Price = 300 }
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);                
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
