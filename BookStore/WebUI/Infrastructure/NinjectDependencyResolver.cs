05b8e5eec849f229a58e54c717b6cc726800f08c
fil-eadr <d.filonenko@eadr.com.ua>
2016-06-02 17:48:33 +0300

    add EF

--- BookStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ BookStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Abstract;
+using Domain.Concrete;
 using Domain.Entities;
 using Moq;
 using Ninject;
@@ -22,14 +23,16 @@ namespace WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IBookRepository> mock = new Mock<IBookRepository>();
-            mock.Setup(m => m.Books).Returns(new List<Book>
-            {
-                new Book {Name = "книга 1", Author = "автор 1", Price = 100},
-                new Book { Name = "книга 2", Author = "автор 2", Price = 200 },
-                new Book { Name = "книга 3", Author = "автор 3", Price = 300 }
-            });
-            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+            //Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            //mock.Setup(m => m.Books).Returns(new List<Book>
+            //{
+            //    new Book {Name = "книга 1", Author = "автор 1", Price = 100},
+            //    new Book { Name = "книга 2", Author = "автор 2", Price = 200 },
+            //    new Book { Name = "книга 3", Author = "автор 3", Price = 300 }
+            //});
+            //kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IBookRepository>().To<EFBookRepository>();
         }
 
         public object GetService(Type serviceType)
